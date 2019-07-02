14769ce909825d2477cf812e8748ef1ad2d75dfe
nicholsonja <nicholsonja@apsu.edu>
2019-07-02 11:11:36 -0500

    update helper for presentation

--- code_example/helper.py
+++ code_example/helper.py
@@ -4,23 +4,26 @@ import numpy as np
 from PIL import Image
 from math import cos, sin
 
-def saveImage(data, imageName, imageWidth, imageHeight):
+def saveImage(data, imageName, imageWidth, imageHeight, bg = None, fg = None):
     '''
     Convert and save data to PNG file. 
     '''
     maxCount = max(data)
-    bg = [255, 255, 255]
-    fg = [  0,   0, 255]
+    if bg == None:
+        bg = [255, 255, 255]
+    
+    if fg == None:  
+        fg = [  0,   0, 255]
     
     rgb = np.zeros((imageWidth, imageHeight, 3), 'uint8')
-    for x in range(imageWidth):
-        for y in range(imageHeight):
+    for y in range(imageHeight):
+        for x in range(imageWidth):
             cnt = data[y * imageWidth + x]
             alpha = min(cnt/maxCount * 10, 1.0)
                 
-            rgb[x, y, 0] = int(alpha * fg[0] + (1 - alpha) * bg[0] + .5)
-            rgb[x, y, 1] = int(alpha * fg[1] + (1 - alpha) * bg[1] + .5)
-            rgb[x, y, 2] = int(alpha * fg[2] + (1 - alpha) * bg[2] + .5)
+            rgb[y, x, 0] = int(alpha * fg[0] + (1 - alpha) * bg[0] + .5)
+            rgb[y, x, 1] = int(alpha * fg[1] + (1 - alpha) * bg[1] + .5)
+            rgb[y, x, 2] = int(alpha * fg[2] + (1 - alpha) * bg[2] + .5)
     
     img = Image.fromarray(rgb)
     img.save(imageName)
